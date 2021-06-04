1cfca620f321a11ebdfaea95d21a28f00d6ce8dc
sangilyoon-dev <sangilyoon.dev@gmail.com>
2021-06-05 02:48:24 +0900

    210605 first

---
+++ ReceiverPractice/app/src/main/java/com/example/receiver/SmsReceiver.java
@@ -0,0 +1,57 @@
+package com.example.receiver;
+
+
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.util.Log;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class SmsReceiver extends BroadcastReceiver {
+	public static final String TAG = "SmsReceiver";
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// TODO: This method is called when the BroadcastReceiver is receiving
+		// an Intent broadcast.
+		Log.i(TAG, "onReceive() 메서드 호출됨");
+
+		Bundle bundle = intent.getExtras();
+		SmsMessage[] messages = parseSmsMessage(bundle);
+
+		if (messages != null && messages.length > 0) {
+			String sender = messages[0].getOriginatingAddress();
+			Log.i(TAG, "SMS sender : " + sender);
+
+			String contents = messages[0].getMessageBody();
+			Log.i(TAG, "SMS contents : " + contents);
+
+			Date receivedDate = new Date(messages[0].getTimestampMillis());
+			Log.i(TAG, "SMS received date : " + receivedDate.toString());
+			}
+		}
+
+		private SmsMessage[] parseSmsMessage(Bundle bundle) {
+			Object[] objs = (Object[]) bundle.get("pdus");
+			SmsMessage[] messages = new SmsMessage[objs.length];
+
+			int smsCount = objs.length;
+			for (int i = 0; i < smsCount; i++) {
+
+				if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+					String format = bundle.getString("format");
+					messages[i] = SmsMessage.createFromPdu((byte[]) objs[i], format);
+				} else {
+					messages[i] = SmsMessage.createFromPdu((byte[]) objs[i]);
+				}
+			}
+
+			return messages;
+		}
+}
